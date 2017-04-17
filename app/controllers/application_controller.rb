1811742ac1f2fa984ad73e9ec1c3bb863a0dd3dd
jon tee <purai3564ef@xvb.biglobe.ne.jp>
2017-04-17 02:04:29 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+  end
+  
   include SessionsHelper
   
   private
